3a47bdb9854ee9bcedf383684075bec4a4a106b8
partho.kunda <partho.kunda@chaldal.net>
2024-08-14 16:16:33 +0600

    Data Extraction Working Next: - Process Folder - Save FileName

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/googleai"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return
+	}
+
+	ctx := context.Background()
+	apiKey := os.Getenv("API_KEY")
+	llm, err := googleai.New(ctx, googleai.WithAPIKey(apiKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// prompt := "What is the L2 Lagrange point?"
+	// answer, err := llms.GenerateFromSinglePrompt(ctx, llm, prompt)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(answer)
+
+	pdfData, err := os.ReadFile("Travel/receipt_8eba0e02-8e68-4b23-9871-a648cb7df4a4.pdf")
+	if err != nil {
+		panic(err)
+	}
+
+	pdfData2, err := os.ReadFile("Travel/receipt_09f8563b-e242-4113-a239-717b9eb9dc87.pdf")
+	if err != nil {
+		panic(err)
+	}
+
+	parts := []llms.ContentPart{
+		llms.BinaryPart("application/pdf", pdfData),
+		llms.BinaryPart("application/pdf", pdfData2),
+		llms.TextPart(`I want to extract the total fare(ignore currency) and the trip date from pdf. Please output the information in the following format: TotalFare_DD-MM-YYYY. For example if the fare is BDT 242.31 and Date 01/01/10 then out output should be 242.31_01-01-10.`),
+	}
+
+	content := []llms.MessageContent{
+		{
+			Role:  llms.ChatMessageType("human"),
+			Parts: parts,
+		},
+	}
+
+	resp, err := llm.GenerateContent(ctx, content, llms.WithModel("gemini-1.5-flash-001"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// bs, _ := json.MarshalIndent(resp, "", "    ")
+	// fmt.Println(resp.Choices[0].Content)
+
+	if len(resp.Choices) <= 0 {
+		fmt.Println("No message received")
+	} else {
+		for _, choice := range resp.Choices {
+			if len(choice.Content) > 0 {
+				fmt.Println(choice.Content)
+			}
+		}
+	}
+	
+}
