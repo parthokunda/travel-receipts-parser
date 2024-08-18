225f32b838cc29f74e4af55fb3cf34bd20b23035
partho.kunda <partho.kunda@chaldal.net>
2024-08-18 23:37:37 +0600

    Folder Receipts Parse by llm. Separated by semicolon

--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import (
 	"os"
 	"path/filepath"
 
-	_ "context"
-	_ "log"
-
-	_ "github.com/joho/godotenv"
-	_ "github.com/tmc/langchaingo/llms"
-	_ "github.com/tmc/langchaingo/llms/googleai"
+	"context"
+	"log"
+	"github.com/joho/godotenv"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/googleai"
 )
 
 type receiptFile struct {
+	fileName	string
 	path 		string
 	content		[]byte
 }
@@ -29,7 +29,7 @@ func get_files(dir string) ([]receiptFile, error) {
 			if err != nil {
 				panic(err)
 			}
-			receipts = append(receipts, receiptFile{path, content})
+			receipts = append(receipts, receiptFile{info.Name(), path, content})
 		}
 		return nil
 	})
@@ -42,63 +42,58 @@ func get_files(dir string) ([]receiptFile, error) {
 }
 
 func main() {
-	recepits, _ := get_files("./Travel")
-	for _, receipt := range recepits {
+	receipts, _ := get_files("./Travel")
+	for _, receipt := range receipts {
 		fmt.Println(receipt.path)
 	}
 	
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	fmt.Println("Error loading .env file:", err)
-	// 	return
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return
+	}
 
-	// ctx := context.Background()
-	// apiKey := os.Getenv("API_KEY")
-	// llm, err := googleai.New(ctx, googleai.WithAPIKey(apiKey))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	ctx := context.Background()
+	apiKey := os.Getenv("API_KEY")
+	llm, err := googleai.New(ctx, googleai.WithAPIKey(apiKey))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// pdfData, err := os.ReadFile("Travel/receipt_8eba0e02-8e68-4b23-9871-a648cb7df4a4.pdf")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	parts := []llms.ContentPart{}
+	
+	for _, receipt := range receipts {
+		parts = append(parts, llms.BinaryPart("application/pdf", receipt.content))
+	}
 
-	// pdfData2, err := os.ReadFile("Travel/receipt_09f8563b-e242-4113-a239-717b9eb9dc87.pdf")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	parts = append(parts, 
+		llms.TextPart(
+			`You have to extract the total fare(ignore currency) and the trip date from pdf. 
+			Please output the information in the following format: DD-MM-YYYY_TotalFare. 
+			For example if the fare is BDT 242.31 and Date 01/01/10 then out output should be 01-01-10_242.31.
+			For multiple files, separate each one with semicolon(;). Keep the original file sequence.
+			Example could be 01-01-10_242.31;31-01-11_192.14`))
 
-	// parts := []llms.ContentPart{
-	// 	llms.BinaryPart("application/pdf", pdfData),
-	// 	llms.BinaryPart("application/pdf", pdfData2),
-	// 	llms.TextPart(`I want to extract the total fare(ignore currency) and the trip date from pdf. Please output the information in the following format: TotalFare_DD-MM-YYYY. For example if the fare is BDT 242.31 and Date 01/01/10 then out output should be 242.31_01-01-10.`),
-	// }
+	content := []llms.MessageContent{
+		{
+			Role:  llms.ChatMessageType("human"),
+			Parts: parts,
+		},
+	}
 
-	// content := []llms.MessageContent{
-	// 	{
-	// 		Role:  llms.ChatMessageType("human"),
-	// 		Parts: parts,
-	// 	},
-	// }
+	resp, err := llm.GenerateContent(ctx, content, llms.WithModel("gemini-1.5-flash-001"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// resp, err := llm.GenerateContent(ctx, content, llms.WithModel("gemini-1.5-flash-001"))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // bs, _ := json.MarshalIndent(resp, "", "    ")
-	// // fmt.Println(resp.Choices[0].Content)
-
-	// if len(resp.Choices) <= 0 {
-	// 	fmt.Println("No message received")
-	// } else {
-	// 	for _, choice := range resp.Choices {
-	// 		if len(choice.Content) > 0 {
-	// 			fmt.Println(choice.Content)
-	// 		}
-	// 	}
-	// }
+	if len(resp.Choices) <= 0 {
+		fmt.Println("No message received")
+	} else {
+		for _, choice := range resp.Choices {
+			if len(choice.Content) > 0 {
+				fmt.Println(choice.Content)
+			}
+		}
+	}
 	
 }
